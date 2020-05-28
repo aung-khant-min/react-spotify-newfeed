3c860d42029821b146c0e51066e9ab0880dcf67d
aung-khant-min <aungkhantmin@ucsm.edu.mm>
2020-05-28 22:21:49 +0630

    Updates

---
+++ precache-manifest.47874c4ec9249a639b816256652c1b5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b66d9064f78325b052143242cec9b3b",
+    "url": "/react-spotify-newfeed/index.html"
+  },
+  {
+    "revision": "33a89264b4cadf4c2590",
+    "url": "/react-spotify-newfeed/static/css/main.8e81631b.chunk.css"
+  },
+  {
+    "revision": "b1107b7deefd446ff7f6",
+    "url": "/react-spotify-newfeed/static/js/2.951978e1.chunk.js"
+  },
+  {
+    "revision": "33a89264b4cadf4c2590",
+    "url": "/react-spotify-newfeed/static/js/main.7cd07a17.chunk.js"
+  },
+  {
+    "revision": "a2a2af5096c310a7c24e",
+    "url": "/react-spotify-newfeed/static/js/runtime-main.d4c0b45e.js"
+  }
+]);
